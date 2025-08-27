c181eaa57b7550ce787d7152da068045e467a39f
belajarngaji <ffarid.5213@gmail.com>
2025-08-27 23:03:35 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -169,4 +169,4 @@ async def health():
 #   MAIN ENTRY (opsional)
 # =======================
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000)
+    uvicorn.run("index:app", host="0.0.0.0", port=5000)
